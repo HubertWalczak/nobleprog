6802a83ebd1b8dca7098818ee783c83fb673708e
Hubert Walczak <228951@edu.p.lodz.pl>
2022-04-11 16:33:41 +0000

    User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_11_162620) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_11_163034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_11_162620) do
     t.string "content"
     t.string "text"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "name"
+    t.text "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
